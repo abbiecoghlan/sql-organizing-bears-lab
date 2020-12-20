6ec66980a6a6d328b3ed86fe27b8ba7c068fbe46
“Abbie <abbie.coghlan@gmail.com>
2020-12-20 15:59:07 -0600

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "chill", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "F", "black", "hyper", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "black", "hyper", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 4, "F", "brown", "lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "brown", "lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "F", "brown", "lazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "M", "black", "hyper", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "F", "black", "hyper", 1);
